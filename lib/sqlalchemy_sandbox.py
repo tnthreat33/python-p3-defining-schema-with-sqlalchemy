2c9d0fa8ffeab7d070994970a28737a9acc42c61
Rachel Hamby <tnthreat33@gmail.com>
2023-11-12 18:27:14 -0500

    redo

--- lib/sqlalchemy_sandbox.py
+++ lib/sqlalchemy_sandbox.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 
-from sqlalchemy import Column, Integer, String, create_engine
+from sqlalchemy import Column, Integer, String, create
 from sqlalchemy.ext.declarative import declarative_base
 
 Base = declarative_base()
